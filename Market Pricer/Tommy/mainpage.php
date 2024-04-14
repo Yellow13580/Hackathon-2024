efb05ccde7e6295c7da2a0f18ba416a58aa569d4
Yellow13580 <89612312+Yellow13580@users.noreply.github.com>
2024-04-14 09:51:28 -0700

    Update mainpage.php
    Changed Marker to Market cause I'm dumb

--- Market Pricer/Tommy/mainpage.php
+++ Market Pricer/Tommy/mainpage.php
@@ -36,7 +36,7 @@
     <div class="firstpage">
         <h1> Welcome To <br /> Market Pricer </h1>
         <p> 
-            Marker Pricer is our free-to-use website that will help you shop quickly and efficiently when you need to split
+            Market Pricer is our free-to-use website that will help you shop quickly and efficiently when you need to split
             the cost of one big shopping trip. If you live with other people and shop together, you can split the cost of 
             certain items allowing for easy budgeting between roommates. All you have to do is list how many people
             you want to split it with and your names! 
